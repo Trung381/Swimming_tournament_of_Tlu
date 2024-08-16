06ea98c1eb86aeb9683d57a4845d5c09bdec988a
Trung <130747543+Trung381@users.noreply.github.com>
2024-08-16 08:42:34 +0700

    hmmm not finished yet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,39 +4,32 @@ import Logo from './components/welcome/logo.js';
 import Banner from './components/welcome/banner.js';
 import LookUpInfor from './components/welcome/lookUpInfor.js';
 import Register from './components/welcome/register.js';
+import Score from './components/home/score.js';
+import InfoManagement from './components/home/info_management.js';
 import { useEffect, useRef } from 'react';
 // import logo_tlu from "../../public/image/logo.png"
 
 function App() {
 
-  const registerRef = useRef(null);
 
-  useEffect(() => {
-
-  }, []);
-
-  function scrollToSection(ref) {
-    if (!ref.current) return;
-    console.log("1111");
-    const yOffset = -50;
-    const y = ref.current.getBoundingClientRect().top + window.scrollY + yOffset;
-    window.scrollTo({top: y, behavior: 'smooth'});
-  }
+  
   
   return (
     <div className="container-fluid">
       <header className="">
 
-        <Logo/>
+        {/* <Logo/>
         <Navbar/>
         <Banner/>
-        <Register/>
-        <div>
+        <Register/> */}
+        {/* <div>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas at ducimus voluptatibus quia nisi corrupti quo nobis cum, doloremque facere sint ipsum soluta dicta quisquam corporis reiciendis eum eos placeat.</p>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ab asperiores, vero eum adipisci aliquid nihil sequi corrupti ipsam laborum est doloremque delectus a. Recusandae, obcaecati molestias. Numquam veritatis illum quo?</p>
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas at ducimus voluptatibus quia nisi corrupti quo nobis cum, doloremque facere sint ipsum soluta dicta quisquam corporis reiciendis eum eos placeat.</p>
           </div>
-        <LookUpInfor/>
+        <LookUpInfor/> */}
+        {/* <Score/> */}
+        <InfoManagement/>
       </header>
     </div>
   );
